db90805feecbaac8b487c83821f41fd1ef041f54
Brittany Isabella Leija <brittanyisabellaleija@Brittanys-Air.attlocal.net>
2024-09-22 17:13:10 -0500

    Added new function in test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
 def hello_world():
     return "Hello, Brittany!"
+def greet_person(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     print(hello_world())
--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 # test_main.py
 import unittest
-from main import hello_world
+from main import hello_world, greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, Brittany!")
+    def test_greet_person(name):
+        name.assertEqual(greet_person(name), f"Hello, {name}!")
+        #Code from fellow student
 
 if __name__ == '__main__':
     unittest.main()
